f2d9e82459f97abd9e6f934265ce75fbc29fc311
KyloRenzz <TheRenzGaming@gmail.com>
2023-09-17 21:47:42 -0500

    Update

--- config.lua
+++ config.lua
@@ -5,9 +5,9 @@ Config.Inventory = 'ox'    ---- ox = Ox-inventory / qb = qb-inventory
 Config.MemberPrice = 1000
 
 Config.Blip = vec3(-1360.336, 160.0437, 57.4211)  -- Blip Location
-Config.PayLoc = vec3(-1359.7, 52.08, 54.44)       -- Where you will Pay for the Membership
-Config.GolfStartLoc = vec3(-1337, 57, 55.3)       -- Where the Location to start Golfing
-Config.CartSpawnLoc = {x = -1324.918701, y = 59.868134, z = 52.7517098}  -- where the cart will spawn
+Config.PayLoc = vec3(-1367.41, 56.54, 53.90)       -- Where you will Pay for the Membership
+Config.GolfStartLoc = vec3(-1348.73, 142.36, 56.27)       -- Where the Location to start Golfing
+Config.CartSpawnLoc = {x = -1351.36, y = 134.05, z = 56.26}  -- where the cart will spawn
 
 
 function alertCops()
